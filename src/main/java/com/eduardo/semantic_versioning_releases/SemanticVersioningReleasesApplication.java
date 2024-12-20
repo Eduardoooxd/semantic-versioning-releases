c5e83c9faf85d6e324d1f058298f36339aea1bc6
Eduardoooxd <ecouto93@gmail.com>
2024-12-20 22:32:57 +0000

    chore: first commit

---
+++ src/main/java/com/eduardo/semantic_versioning_releases/SemanticVersioningReleasesApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.semantic_versioning_releases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticVersioningReleasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticVersioningReleasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/semantic_versioning_releases/SemanticVersioningReleasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.semantic_versioning_releases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanticVersioningReleasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
