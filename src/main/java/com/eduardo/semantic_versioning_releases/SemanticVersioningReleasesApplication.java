abc3a1d68f16d1bd35bd1b311fa8fa6bb0256da7
Eduardoooxd <ecouto93@gmail.com>
2024-12-20 23:09:58 +0000

    chore: adding comments

--- src/main/java/com/eduardo/semantic_versioning_releases/SemanticVersioningReleasesApplication.java
+++ src/main/java/com/eduardo/semantic_versioning_releases/SemanticVersioningReleasesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SemanticVersioningReleasesApplication {
 
 	public static void main(String[] args) {
-		// Spring application
+		// Spring application 1
 		SpringApplication.run(SemanticVersioningReleasesApplication.class, args);
 
 	}
