29f205a4387483e90f7f9d130d377a15815a80ef
Eduardoooxd <ecouto93@gmail.com>
2024-12-20 23:48:44 +0000

    feat: added logs

--- src/main/java/com/eduardo/semantic_versioning_releases/SemanticVersioningReleasesApplication.java
+++ src/main/java/com/eduardo/semantic_versioning_releases/SemanticVersioningReleasesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SemanticVersioningReleasesApplication {
 
 	public static void main(String[] args) {
-		// Spring application 1
+		// Spring application 2
 		SpringApplication.run(SemanticVersioningReleasesApplication.class, args);
 
 	}
